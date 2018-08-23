1568a19933d8a78c02fdb99da1d64733b9f9033f
zcx <zcx@umich.edu>
2018-08-23 14:14:01 -0400

    xyz_sfetch uses indexing

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ TMalign: TMalign.cpp param_set.h basic_fun.h Kabsch.h NW.h TMalign.h pstream.h
 pdb2xyz: pdb2xyz.cpp basic_fun.h pstream.h
 	${CC} ${CFLAGS} $@.cpp -o $@ ${LDFLAGS}
 
-xyz_sfetch: xyz_sfetch.cpp pstream.h
+xyz_sfetch: xyz_sfetch.cpp
 	${CC} ${CFLAGS} $@.cpp -o $@ ${LDFLAGS}
 
 clean:
--- xyz_sfetch.cpp
+++ xyz_sfetch.cpp
@@ -1,6 +1,16 @@
+/* xyz_sfetch - index and fetch entries from multi-chain xyz file.
+ *     The time of indexing operation scales linearly with the size of xyz
+ * file. Indexing is very fast (~2 s for PDB70 library with ~70000 entries).
+ *     The time of fetching operation scales linearly with the length of
+ * entry list, but is almost unrelated to the size of xyz file. Therefore,
+ * this program is designed for fetch a small number of entries from a
+ * big xyz file. Fetching the whole PDB70 from PDB70 itself take ~0.5 min;
+ * fetching 1000 entries from PDB70 only takes 1~2 s */
 #include <iostream>
 #include <fstream>
-#include "pstream.h"
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,12 +18,12 @@ void print_help()
 {
     cout <<
 "Usage: xyz_sfetch ca.xyz\n"
-"    List all entries in xyz file 'ca.xyz'\n"
-"    them to 'subset.xyz'\n"
+"    Index all entries in xyz file 'ca.xyz' into 'ca.xyz.index'.\n"
+"    Output file name inferred from input file name.\n"
 "\n"
 "Usage: xyz_sfetch ca.xyz list > subset.xyz\n"
-"    From xyz file 'ca.xyz', extract all entries listed by 'list'. Output\n"
-"    them to 'subset.xyz'\n"
+"    From xyz file 'ca.xyz' and respective index file 'ca.xyz.index',\n"
+"    fetch all entries listed by 'list'. Output them to 'subset.xyz'.\n"
     <<endl;
     exit(EXIT_SUCCESS);
 }
@@ -21,7 +31,6 @@ void print_help()
 int main(int argc, char *argv[])
 {
     if (argc < 2) print_help();
-
 
     /* get argument */
     string filename="";
@@ -34,49 +43,38 @@ int main(int argc, char *argv[])
     }
 
     if(filename.size()==0||filename=="-h") print_help();
+    if ((filename.size()>=3 && filename.substr(filename.size()-3,3)==".gz")||
+        (filename.size()>=4 && filename.substr(filename.size()-4,4)==".bz2"))
+    {
+        cerr<<"ERROR! This program does not support .gz or .bz2 file"<<endl;
+        exit(EXIT_SUCCESS);
+    }
 
-    /* Check xyz file compress type */
-    int compress_type=0;   // uncompressed file
-    ifstream fin;
-    redi::ipstream fin_gz; // if file is compressed
-    if (filename.size()>=3 && 
-        filename.substr(filename.size()-3,3)==".gz")
-    {
-        fin_gz.open("zcat "+filename);
-        compress_type=1;
-    }
-    else if (filename.size()>=4 && 
-        filename.substr(filename.size()-4,4)==".bz2")
-    {
-        fin_gz.open("bzcat "+filename);
-        compress_type=2;
-    }
-    else fin.open(filename.c_str());
+    ifstream fin(filename.c_str());
 
     /* list all entries in xyz file */
     string line;
     int L,i;
+    int start_pos,end_pos; // position of starting and ending character
     if (list_opt.size()==0)
     {
-        while (compress_type?fin_gz.good():fin.good())
+        ofstream fp((filename+".index").c_str());
+        while (fin.good())
         {
-            if (compress_type) getline(fin_gz, line);
-            else               getline(fin, line);
+            start_pos=fin.tellg();
+            getline(fin, line);
             L=atoi(line.c_str());
-            if (compress_type) getline(fin_gz, line);
-            else               getline(fin, line);
-            for (i=0;i<line.size();i++)
-                if (line[i]==' '||line[i]=='\t') break;
-            if (!(compress_type?fin_gz.good():fin.good())) break;
+            getline(fin, line);
+            if (!fin.good()) break;
+            for(i=0;i<line.size();i++) if(line[i]==' '||line[i]=='\t') break;
             cout<<line.substr(0,i)<<'\t'<<L<<endl;
-            for (i=0;i<L;i++)
-            {
-                if (compress_type) getline(fin_gz, line);
-                else               getline(fin, line);
-            }
+            fp<<line.substr(0,i)<<'\t'<<start_pos;
+            for (i=0;i<L;i++) getline(fin, line);
+            end_pos=fin.tellg();
+            fp<<'\t'<<end_pos<<endl;
         }
-        if (compress_type) fin_gz.close();
-        else               fin.close();
+        fin.close();
+        fp.close();
         /* clean up */
         line.clear();
         filename.clear();
@@ -96,40 +94,38 @@ int main(int argc, char *argv[])
     }
     fp.close();
 
-    /* extract listed entries in xyz file */
-    string txt;
-    while (compress_type?fin_gz.good():fin.good())
+    /* read xyz index */
+    /* In xyz file, each line has 28 chacters plus an additional '\n'. In PDB
+     * file, a residue number has up to 4 digits, which means a PDB chain
+     * usually has up to 9999 residues. 29*9999 == 289971 < 300000 */
+    fp.open((filename+".index").c_str());
+    if (!fp.is_open())
     {
-        if (compress_type) getline(fin_gz, line);
-        else               getline(fin, line);
-        L=atoi(line.c_str());
-        txt+=line+'\n';
-        if (compress_type) getline(fin_gz, line);
-        else               getline(fin, line);
-        for (i=0;i<line.size();i++)
-            if (line[i]==' '||line[i]=='\t') break;
-        if (!(compress_type?fin_gz.good():fin.good())) break;
-        if (find(chain_list.begin(), chain_list.end(),
-            line.substr(0,i))==chain_list.end())
-        {
-            txt.clear();
-            continue;
-        }
-        txt+=line;
-        for (i=0;i<L;i++)
-        {
-            if (compress_type) getline(fin_gz, line);
-            else               getline(fin, line);
-            txt+='\n'+line;
-        }
-        cout<<txt<<endl;
-        txt.clear();
+        cerr<<"ERROR! No index file at "+filename+".index"<<endl;
+        cerr<<"Run the following command to create the index file:"<<endl;
+        cerr<<argv[0]<<" "<<filename<<endl;
+        exit(EXIT_SUCCESS);
     }
-    if (compress_type) fin_gz.close();
-    else               fin.close();
+    char *buf=new char [300000];
+    string chain;
+    while (fp.good())
+    {
+        fp>>chain>>start_pos>>end_pos;
+        if (!fp.good()) break;
+        if (find(chain_list.begin(), chain_list.end(), 
+            chain)==chain_list.end()) continue;
+        fin.seekg(start_pos);
+        fin.read(buf,end_pos-start_pos);
+        buf[end_pos-start_pos]=0; // ensures old text beyond this is ignored
+        cout<<buf;
+    }
+    fp.close();
+    fin.close();
+
     /* clean up */
-    txt.clear();
-    line.clear();
+    /* No need to flush, because any input from cin, output to cerr, or
+     * or program termination forces cout.flush() */
+    delete[]buf;
     filename.clear();
     list_opt.clear();
     chain_list.clear();
