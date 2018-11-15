bddbe9f338251d4a8f6dbb380825439c64a611d4
zcx <zcx@umich.edu>
2018-11-15 10:20:41 -0500

    add pdb2ss

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=g++
 CFLAGS=-O3 -ffast-math
 LDFLAGS=-static# -lm
-PROGRAM=TMalign se pdb2xyz xyz_sfetch pdb2fasta
+PROGRAM=TMalign se pdb2xyz xyz_sfetch pdb2fasta pdb2ss
 
 all: ${PROGRAM}
 
@@ -9,6 +9,9 @@ TMalign: TMalign.cpp param_set.h basic_fun.h Kabsch.h NW.h TMalign.h pstream.h
 	${CC} ${CFLAGS} $@.cpp -o $@ ${LDFLAGS}
 
 se: se.cpp se.h param_set.h basic_fun.h Kabsch.h NW.h TMalign.h pstream.h
+	${CC} ${CFLAGS} $@.cpp -o $@ ${LDFLAGS}
+
+pdb2ss: pdb2ss.cpp se.h param_set.h basic_fun.h Kabsch.h NW.h TMalign.h pstream.h
 	${CC} ${CFLAGS} $@.cpp -o $@ ${LDFLAGS}
 
 pdb2xyz: pdb2xyz.cpp basic_fun.h pstream.h
---
+++ pdb2ss.cpp
@@ -0,0 +1,213 @@
+#include "TMalign.h"
+
+using namespace std;
+
+// secondary structure    01234
+const char* SSmapProtein=" CHTE";
+const char* SSmapRNA    =" ><.";
+
+void print_help()
+{
+    cout <<
+"Converting PDB file(s) into FASTA format secondary structure sequence.\n"
+"Proteins have four states: H E C T (helix, strand, coil, turn)\n"
+"RNA ave three states: < > . (paired with 3', paired with 5', unpaired)\n"
+"\n"
+"Usage: pdb2ss pdb.pdb > seq.ss\n"
+"\n"
+"    -dir     Convert all chains listed by 'chain_list' under 'chain_folder'.\n"
+"             Note that the slash is necessary.\n"
+"             $ pdb2xyz -dir chain_folder/ chain_list\n"
+"\n"
+"    -suffix  (Only when -dir is set, default is empty)\n"
+"             add file name suffix to files listed by chain_list\n"
+"\n"
+"    -atom    4-character atom name used to represent a residue.\n"
+"             Default is \" C3'\" for RNA/DNA and \" CA \" for proteins\n"
+"             (note the spaces before and after CA).\n"
+"\n"
+"    -mol     Molecule type: RNA or protein\n"
+"             Default is detect molecule type automatically\n"
+"\n"
+"    -ter     Strings to mark the end of a chain\n"
+"             3: (default) TER, ENDMDL, END or different chain ID\n"
+"             2: ENDMDL, END, or different chain ID\n"
+"             1: ENDMDL or END\n"
+"             0: end of file\n"
+"\n"
+"    -split   Whether to split PDB file into multiple chains\n"
+"             0: (default) treat the whole structure as one single chain\n"
+"             1: treat each MODEL as a separate chain (-ter should be 0)\n"
+"             2: treat each chain as a seperate chain (-ter should be <=1)\n"
+"\n"
+"    -infmt   Input format for chain2\n"
+"             0: (default) PDB format\n"
+"             2: xyz format\n"
+    <<endl;
+    exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) print_help();
+
+
+    /**********************/
+    /*    get argument    */
+    /**********************/
+    string xname     = "";
+    int    ter_opt   =3;     // TER, END, or different chainID
+    int    infmt_opt =0;     // PDB format
+    int    split_opt =0;     // do not split chain
+    string atom_opt  ="auto";// use C alpha atom for protein and C3' for RNA
+    string mol_opt   ="auto";// auto-detect the molecule type as protein/RNA
+    string suffix_opt="";    // set -suffix to empty
+    string dir_opt   ="";    // set -dir to empty
+    vector<string> chain_list; // only when -dir1 is set
+
+    int nameIdx = 0;
+    for(int i = 1; i < argc; i++)
+    {
+        if ( !strcmp(argv[i],"-ter") && i < (argc-1) )
+        {
+            ter_opt=atoi(argv[i + 1]); i++;
+        }
+        else if ( !strcmp(argv[i],"-split") && i < (argc-1) )
+        {
+            split_opt=atoi(argv[i + 1]); i++;
+        }
+        else if ( !strcmp(argv[i],"-atom") && i < (argc-1) )
+        {
+            atom_opt=argv[i + 1]; i++;
+        }
+        else if ( !strcmp(argv[i],"-mol") && i < (argc-1) )
+        {
+            mol_opt=argv[i + 1]; i++;
+        }
+        else if ( !strcmp(argv[i],"-dir") && i < (argc-1) )
+        {
+            dir_opt=argv[i + 1]; i++;
+        }
+        else if ( !strcmp(argv[i],"-suffix") && i < (argc-1) )
+        {
+            suffix_opt=argv[i + 1]; i++;
+        }
+        else if ( !strcmp(argv[i],"-infmt") && i < (argc-1) )
+        {
+            infmt_opt=atoi(argv[i + 1]); i++;
+        }
+        else xname=argv[i];
+    }
+
+    if(xname.size()==0||xname=="-h") print_help();
+
+    if (suffix_opt.size() && dir_opt.size()==0)
+        PrintErrorAndQuit("-suffix is only valid if -dir is set");
+    if (atom_opt.size()!=4)
+        PrintErrorAndQuit("ERROR! atom name must have 4 characters, including space.");
+    if (mol_opt!="auto" && mol_opt!="protein" && mol_opt!="RNA")
+        PrintErrorAndQuit("ERROR! molecule type must be either RNA or protein.");
+    if (split_opt==1 && ter_opt!=0)
+        PrintErrorAndQuit("-split 1 should be used with -ter 0");
+    else if (split_opt==2 && ter_opt!=0 && ter_opt!=1)
+        PrintErrorAndQuit("-split 2 should be used with -ter 0 or 1");
+
+    /* parse file list */
+    if (dir_opt.size()==0)
+        chain_list.push_back(xname);
+    else
+    {
+        ifstream fp(xname.c_str());
+        if (! fp.is_open())
+        {
+            char message[5000];
+            sprintf(message, "Can not open file: %s\n", xname.c_str());
+            PrintErrorAndQuit(message);
+        }
+        string line;
+        while (fp.good())
+        {
+            getline(fp, line);
+            if (! line.size()) continue;
+            chain_list.push_back(dir_opt+Trim(line)+suffix_opt);
+        }
+        fp.close();
+        line.clear();
+    }
+
+    /* declare previously global variables */
+    vector<vector<string> >PDB_lines; // text of chain
+    vector<int> mol_vec;              // molecule type of chain
+    vector<string> chainID_list;      // list of chainID1
+    int    i;                         // file index
+    int    l;                         // residue index
+    int    chain_i;                   // chain index
+    int    xlen;                      // chain length
+    int    xchainnum;                 // number of chains in a PDB file
+    char   *seqx;                     // for the protein sequence 
+    int    *secx;                     // for the secondary structure 
+    int    *xresno;                   // residue number
+    double **xa;                       // for input vectors xa[0...xlen-1][0..2] and
+    vector<string> resi_vec;          // residue index for chain
+    string sequence;                  // secondary structure sequence
+
+    /* loop over file names */
+    for (i=0;i<chain_list.size();i++)
+    {
+        xname=chain_list[i];
+        xchainnum=get_PDB_lines(xname, PDB_lines, chainID_list,
+            resi_vec, mol_vec, 0, ter_opt, infmt_opt, atom_opt, split_opt);
+        if (!xchainnum)
+        {
+            cerr<<"Warning! Cannot parse file: "<<xname
+                <<". Chain number 0."<<endl;
+            continue;
+        }
+        for (chain_i=0;chain_i<xchainnum;chain_i++)
+        {
+            xlen=PDB_lines[chain_i].size();
+            if (mol_opt=="RNA") mol_vec[chain_i]=1;
+            else if (mol_opt=="protein") mol_vec[chain_i]=-1;
+            if (!xlen)
+            {
+                cerr<<"Warning! Cannot parse file: "<<xname
+                    <<". Chain length 0."<<endl;
+                continue;
+            }
+            NewArray(&xa, xlen, 3);
+            seqx = new char[xlen + 1];
+            secx = new int[xlen];
+            xresno = new int[xlen];
+            xlen = read_PDB(PDB_lines[chain_i], xa, seqx, xresno);
+            if (mol_vec[chain_i]>0) // RNA
+            {
+                make_sec(seqx,xa, xlen, secx,atom_opt);
+                for (l=0;l<PDB_lines[chain_i].size();l++)
+                    sequence+=SSmapRNA[secx[l]];
+            }
+            else //protein
+            {
+                make_sec(xa, xlen, secx);
+                for (l=0;l<PDB_lines[chain_i].size();l++)
+                    sequence+=SSmapProtein[secx[l]];
+            }
+            
+            cout<<'>'<<xname.substr(dir_opt.size(),
+                xname.size()-dir_opt.size()-suffix_opt.size())
+                <<chainID_list[chain_i]<<'\t'<<xlen<<'\n'<<sequence<<endl;
+
+            sequence.clear();
+            PDB_lines[chain_i].clear();
+            DeleteArray(&xa, xlen);
+            delete [] seqx;
+            delete [] secx;
+            delete [] xresno;
+        } // chain_i
+        xname.clear();
+        PDB_lines.clear();
+        resi_vec.clear();
+        mol_vec.clear();
+    } // i
+    chain_list.clear();
+    return 0;
+}
