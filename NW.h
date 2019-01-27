57cb9126874b9f4e861cf4ae730fef90a859efe5
zcx <zcx@xenial>
2019-01-27 02:08:29 -0500

    fix asymmetric for 1se9A vs 2edpA

--- NW.h
+++ NW.h
@@ -10,29 +10,29 @@
  * TMalign A.pdb B.pdb and TMalign B.pdb A.pdb have different TM_A and TM_B
  * values) caused by the NWPD_TM implement.
  */
-void NWDP_TM( double **score, bool **path, double **val,
+
+/* Input: score[1:len1, 1:len2], and gap_open
+ * Output: j2i[1:len2] \in {1:len1} U {-1}
+ * path[0:len1, 0:len2]=1,2,3, from diagonal, horizontal, vertical */
+void NWDP_TM(double **score, bool **path, double **val,
     int len1, int len2, double gap_open, int j2i[])
 {
-    //NW dynamic programming for alignment
-    //not a standard implementation of NW algorithm
-    //Input: score[1:len1, 1:len2], and gap_open
-    //Output: j2i[1:len2] \in {1:len1} U {-1}
-    //path[0:len1, 0:len2]=1,2,3, from diagonal, horizontal, vertical
 
     int i, j;
     double h, v, d;
 
     //initialization
-    val[0][0]=0;
     for(i=0; i<=len1; i++)
     {
-        val[i][0]=0;
+        //val[i][0]=0;
+        val[i][0]=i*gap_open;
         path[i][0]=false; //not from diagonal
     }
 
     for(j=0; j<=len2; j++)
     {
-        val[0][j]=0;
+        //val[0][j]=0;
+        val[0][j]=j*gap_open;
         path[0][j]=false; //not from diagonal
         j2i[j]=-1;    //all are not aligned, only use j2i[1:len2]
     }      
@@ -47,13 +47,11 @@ void NWDP_TM( double **score, bool **path, double **val,
 
             //symbol insertion in horizontal (= a gap in vertical)
             h=val[i-1][j];
-            if(path[i-1][j]) //aligned in last position
-                h += gap_open;
+            if(path[i-1][j]) h += gap_open; //aligned in last position
 
             //symbol insertion in vertical
             v=val[i][j-1];
-            if(path[i][j-1]) //aligned in last position
-                v += gap_open;
+            if(path[i][j-1]) v += gap_open; //aligned in last position
 
 
             if(d>=h && d>=v)
@@ -64,10 +62,8 @@ void NWDP_TM( double **score, bool **path, double **val,
             else 
             {
                 path[i][j]=false; //from horizontal
-                if(v>=h)
-                    val[i][j]=v;
-                else
-                    val[i][j]=h;
+                if(v>=h) val[i][j]=v;
+                else val[i][j]=h;
             }
         } //for i
     } //for j
@@ -91,38 +87,34 @@ void NWDP_TM( double **score, bool **path, double **val,
             v=val[i][j-1];
             if(path[i][j-1]) v +=gap_open;
 
-            if(v>=h)
-                j--;
-            else
-                i--;
+            if(v>=h) j--;
+            else i--;
         }
     }
 }
 
-void NWDP_TM( double **score, bool **path, double **val,
-    double **x, double **y, int len1, int len2, double t[3], double u[3][3],
+/* Input: vectors x, y, rotation matrix t, u, scale factor d02, and gap_open
+ * Output: j2i[1:len2] \in {1:len1} U {-1}
+ * path[0:len1, 0:len2]=1,2,3, from diagonal, horizontal, vertical */
+void NWDP_TM( bool **path, double **val, double **x, double **y,
+    int len1, int len2, double t[3], double u[3][3],
     double d02, double gap_open, int j2i[])
 {
-    //NW dynamic programming for alignment
-    //not a standard implementation of NW algorithm
-    //Input: vectors x, y, rotation matrix t, u, scale factor d02, and gap_open
-    //Output: j2i[1:len2] \in {1:len1} U {-1}
-    //path[0:len1, 0:len2]=1,2,3, from diagonal, horizontal, vertical
-
     int i, j;
     double h, v, d;
 
     //initialization
-    val[0][0]=0;
     for(i=0; i<=len1; i++)
     {
-        val[i][0]=0;
+        //val[i][0]=0;
+        val[i][0]=i*gap_open;
         path[i][0]=false; //not from diagonal
     }
 
     for(j=0; j<=len2; j++)
     {
-        val[0][j]=0;
+        //val[0][j]=0;
+        val[0][j]=j*gap_open;
         path[0][j]=false; //not from diagonal
         j2i[j]=-1;    //all are not aligned, only use j2i[1:len2]
     }      
@@ -135,19 +127,16 @@ void NWDP_TM( double **score, bool **path, double **val,
         transform(t, u, &x[i-1][0], xx);
         for(j=1; j<=len2; j++)
         {
-            //d=val[i-1][j-1]+score[i][j]; //diagonal
             dij=dist(xx, &y[j-1][0]);    
             d=val[i-1][j-1] +  1.0/(1+dij/d02);
 
             //symbol insertion in horizontal (= a gap in vertical)
             h=val[i-1][j];
-            if(path[i-1][j]) //aligned in last position
-                h += gap_open;
+            if(path[i-1][j]) h += gap_open; //aligned in last position
 
             //symbol insertion in vertical
             v=val[i][j-1];
-            if(path[i][j-1]) //aligned in last position
-                v += gap_open;
+            if(path[i][j-1]) v += gap_open; //aligned in last position
 
 
             if(d>=h && d>=v)
@@ -158,10 +147,8 @@ void NWDP_TM( double **score, bool **path, double **val,
             else 
             {
                 path[i][j]=false; //from horizontal
-                if(v>=h)
-                    val[i][j]=v;
-                else
-                    val[i][j]=h;
+                if(v>=h) val[i][j]=v;
+                else val[i][j]=h;
             }
         } //for i
     } //for j
@@ -185,39 +172,35 @@ void NWDP_TM( double **score, bool **path, double **val,
             v=val[i][j-1];
             if(path[i][j-1]) v +=gap_open;
 
-            if(v>=h)
-                j--;
-            else
-                i--;
+            if(v>=h) j--;
+            else i--;
         }
     }
 }
 
-//+ss
-void NWDP_TM(double **score, bool **path, double **val,
-    const int *secx, const int *secy, const int len1, const int len2,
-    const double gap_open, int j2i[])
+/* +ss
+ * Input: secondary structure secx, secy, and gap_open
+ * Output: j2i[1:len2] \in {1:len1} U {-1}
+ * path[0:len1, 0:len2]=1,2,3, from diagonal, horizontal, vertical */
+void NWDP_TM(bool **path, double **val, const int *secx, const int *secy,
+    const int len1, const int len2, const double gap_open, int j2i[])
 {
-    //NW dynamic programming for alignment
-    //not a standard implementation of NW algorithm
-    //Input: secondary structure secx, secy, and gap_open
-    //Output: j2i[1:len2] \in {1:len1} U {-1}
-    //path[0:len1, 0:len2]=1,2,3, from diagonal, horizontal, vertical
 
     int i, j;
     double h, v, d;
 
     //initialization
-    val[0][0]=0;
     for(i=0; i<=len1; i++)
     {
-        val[i][0]=0;
+        //val[i][0]=0;
+        val[i][0]=i*gap_open;
         path[i][0]=false; //not from diagonal
     }
 
     for(j=0; j<=len2; j++)
     {
-        val[0][j]=0;
+        //val[0][j]=0;
+        val[0][j]=j*gap_open;
         path[0][j]=false; //not from diagonal
         j2i[j]=-1;    //all are not aligned, only use j2i[1:len2]
     }      
@@ -227,26 +210,15 @@ void NWDP_TM(double **score, bool **path, double **val,
     {
         for(j=1; j<=len2; j++)
         {
-            //d=val[i-1][j-1]+score[i][j]; //diagonal
-            if(secx[i-1]==secy[j-1])
-            {
-                d=val[i-1][j-1] + 1.0;
-            }
-            else
-            {
-                d=val[i-1][j-1];
-            }
+            d=val[i-1][j-1] + 1.0*(secx[i-1]==secy[j-1]);
 
             //symbol insertion in horizontal (= a gap in vertical)
             h=val[i-1][j];
-            if(path[i-1][j]) //aligned in last position
-                h += gap_open;
+            if(path[i-1][j]) h += gap_open; //aligned in last position
 
             //symbol insertion in vertical
             v=val[i][j-1];
-            if(path[i][j-1]) //aligned in last position
-                v += gap_open;
-
+            if(path[i][j-1]) v += gap_open; //aligned in last position
 
             if(d>=h && d>=v)
             {
@@ -256,10 +228,8 @@ void NWDP_TM(double **score, bool **path, double **val,
             else 
             {
                 path[i][j]=false; //from horizontal
-                if(v>=h)
-                    val[i][j]=v;
-                else
-                    val[i][j]=h;
+                if(v>=h) val[i][j]=v;
+                else val[i][j]=h;
             }
         } //for i
     } //for j
@@ -283,10 +253,8 @@ void NWDP_TM(double **score, bool **path, double **val,
             v=val[i][j-1];
             if(path[i][j-1]) v +=gap_open;
 
-            if(v>=h)
-                j--;
-            else
-                i--;
+            if(v>=h) j--;
+            else i--;
         }
     }
 }
