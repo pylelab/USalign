38fb294ed437fa7216f1c50f5103bab0be43fc10
zcx <zcx@xenial>
2022-09-24 17:40:46 -0400

    more consistent check of USalign in pymol

--- usalign.py
+++ usalign.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env pymol
 '''
 PyMOL plugin for US-align
 
@@ -22,15 +23,32 @@ INSTALLATION
 from __future__ import print_function
 
 __author__ = 'Chengxin Zhang'
-__version__ = '20220830'
+__version__ = '20220924'
 __license__ = 'BSD-2-Clause'
 
 from pymol import cmd, CmdException
 import subprocess
 import tempfile
 import os
+import platform
 
-def usalign(mobile, target, args='', exe='USalign', transform=1):
+def get_usalign_path(exe="USalign"):
+    if platform.system().lower().startswith("win"):
+        exe+=".exe"
+    filename = os.path.join(os.path.dirname(os.path.abspath(__file__)),exe)
+    if os.path.isfile(filename):
+        return filename
+    else:
+        for p in os.getenv("PATH").split(os.pathsep):
+            filename=os.path.join(p,exe)
+            if os.path.isfile(filename):
+                return filename
+    print("ERROR! Cannot locate %s at %s or at %s"%(exe,
+        os.path.dirname(os.path.abspath(__file__)),os.getenv("PATH")))
+    print("Please put the USalign executable at one of the aforementioned paths")
+    return exe
+
+def usalign(mobile, target, args='', exe='', transform=1):
     '''
 USAGE
 
@@ -61,7 +79,8 @@ CITATION
     cmd.save(mobile_filename, mobile_ca_sele)
     cmd.save(target_filename, target_ca_sele)
 
-    exe = cmd.exp_path(exe)
+    if len(exe)==0:
+        exe=get_usalign_path("USalign")
     if args=='""':
         args=''
     if len(args)>2 and args[0]=='"' and args[-1]=='"':
@@ -72,7 +91,7 @@ CITATION
     print(args)
 
     try:
-        process = subprocess.Popen(args, stdout=subprocess.PIPE,
+        process = subprocess.Popen(args, stdout=subprocess.PIPE, shell=True,
                 universal_newlines=True)
         lines = process.stdout.readlines()
     except OSError:
@@ -104,11 +123,10 @@ CITATION
 
 # pymol commands
 cmd.extend('usalign', usalign)
+cmd.extend('USalign', usalign)
 
 # autocompletion
-cmd.auto_arg[0].update({
-    'usalign': cmd.auto_arg[0]['align'],
-})
-cmd.auto_arg[1].update({
-    'usalign': cmd.auto_arg[1]['align'],
-})
+cmd.auto_arg[0].update({ 'usalign': cmd.auto_arg[0]['align'], })
+cmd.auto_arg[1].update({ 'usalign': cmd.auto_arg[1]['align'], })
+cmd.auto_arg[0].update({ 'USalign': cmd.auto_arg[0]['align'], })
+cmd.auto_arg[1].update({ 'USalign': cmd.auto_arg[1]['align'], })
